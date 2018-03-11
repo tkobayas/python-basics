3631c5134d17ae12b6bb3326bc7d90e962c6e532
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-03-11 14:31:43 +0900

    if

---
+++ hamegg/flow/if.py
@@ -0,0 +1,5 @@
+remainder = 10 % 3
+
+if remainder != 0:
+    print "not divisible"
+    print "remainder = ",  remainder
---
+++ hamegg/flow/if2.py
@@ -0,0 +1,14 @@
+print "Start"
+x = 0
+y = 1
+
+if x == 0:
+    print "x = 0"
+    
+    if y == 0:
+        print "y = 0"
+        
+    if y != 0:
+        print "y != 0"
+
+print "End"
