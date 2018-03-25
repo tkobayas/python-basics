f742ad4d672486a0a8ae1eca3ad63a9a24a87ce8
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-03-25 20:49:58 +0900

    numpy 1st

---
+++ hamegg/numpy/array.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+arr = np.asarray([1, 2, 3])
+
+print arr
+print arr.shape #shape is represented as tuple
+
+print "-----"
+
+arr = np.asarray([[1, 2, 3], [4, 5, 6]])
+
+print arr
+print arr.shape #shape is represented as tuple
+
+print "-----"
+
+arr = np.zeros((2, 3))
+
+print arr
+
+print "-----"
+
+arr = np.ones((2, 3))
+
+print arr
+
+print "-----"
+
+arr = np.ones((2, 3))
+
+print arr
+
+print "-----"
+
+arr = np.random.rand(2, 3)
+
+print arr
+
+print "-----"
+
+arr = np.random.randn(2, 3) # random as normal distribution
+
+print arr
+
+print "-----"
