9c74d04f3c7d91cf2fceefbc5ae2dd4739d715bf
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-04-22 12:19:15 +0900

    dot

--- numpy/array/array-dot.py
+++ numpy/array/array-dot.py
@@ -1,23 +1,11 @@
 import numpy as np
 
-a = np.array([[1, 2, 3]])
-b = np.array([[4, 5, 6]])
 
-c = np.multiply(a, b)
-
-print a
-print a.shape
-print b
-print b.shape
-print c
-print c.shape
-
-print '-------------'
 
 a = np.array([[1, 2, 3]])
 b = np.array([[4], [5], [6]])
 
-c = np.multiply(a, b)
+c = np.dot(a, b)
 
 print a
 print a.shape
@@ -31,21 +19,7 @@ print '-------------'
 a = np.array([[1], [2], [3]])
 b = np.array([[4, 5, 6]])
 
-c = np.multiply(a, b)
-
-print a
-print a.shape
-print b
-print b.shape
-print c
-print c.shape
-
-print '-------------'
-
-a = np.array([[1], [2], [3]])
-b = np.array([[4], [5], [6]])
-
-c = np.multiply(a, b)
+c = np.dot(a, b)
 
 print a
 print a.shape
@@ -59,7 +33,7 @@ print '-------------'
 a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 b = np.array([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
 
-c = np.multiply(a, b)
+c = np.dot(a, b)
 
 print a
 print a.shape
